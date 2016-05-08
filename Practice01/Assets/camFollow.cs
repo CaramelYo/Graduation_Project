affde514264f8721edb6bb0737f53b1bfa7f9edc
F74024054 <benwang0912@gmail.com>
2016-05-08 21:31:57 +0800

    sinpin test commit

---
+++ Practice01/Assets/camFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollow : MonoBehaviour {
+    public GameObject target;
+    public float damping = 1;
+    Vector3 distance;
+
+    void Start()
+    {
+        distance = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + distance;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+}
